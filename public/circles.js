78ec0460e3c91abf38cd62a3df7673a0cb51ca8c
Evguenia Usoskina <jane.usoskina@gmail.com>
2020-02-15 15:52:47 +0000

    added circles

--- public/circles.js
+++ public/circles.js
@@ -1,52 +1,38 @@
-// lines drawn diagonally
+var sets = 100;
+var x;
+var y;
+var circles;
 
-let gifLength = 1800;
-let canvas;
-
-let x1 = 0;
-let y1 = 0;
-let x2 = 30;
-let y2 = 30;
-paintColour = 255;
-additionPaint = 1;
-
-
-function setup() {
-  var p5canvas = createCanvas(400, 400);
-  canvas = p5canvas.canvas;
-    background(0,179,255);
-  
-  capturer.start();
-
+function setup(){
+  createCanvas(400,400,SVG);
+  noStroke();
+  fill(255)
+  background(255);
 }
 
-function draw() {
-  stroke(paintColour);
-    strokeWeight(2); // Thicker
-    line(x1, y1, x2, y2);
-  if(x1 < 390) {
-    x1 = x1+30;
-  } else {
-    x1 = 0;
-  }
-  if(y1 < 390){
-    y1 = y1+3;
-  }else{
-    y1 = 0;
-  }
-    y2 = y1+30;
-    x2 = x1+30;
+function draw(){
+  for(var i = 0; i < sets; ++i){
+    x = Math.floor(Math.random()*width);
+    y = Math.floor(Math.random()*height);
+    multiplier = Math.floor(Math.random()*30)
+    x_off = Math.floor(Math.random()*6-3)
+    y_off = Math.floor(Math.random()*6-3)
 
-    if(paintColour === 0 || paintColour === 255){
-      additionPaint = additionPaint * -1;
+    //number of circles
+    circles = Math.floor(Math.random()*10);
+    //for each circle
+    for (var total = 0; total < circles; total = total+1) {
+      //compute circle diameter based on reverse index (circles-total) (or max-current)
+      var diameter = (circles-total) * multiplier;
+      //fill(total * 30);
+      stroke(0)
+      //render the circle
+      ellipse(x,y, diameter, diameter);
+      x = x+x_off
+      y = y+y_off
+      
     }
-    paintColour = paintColour+additionPaint;
-
-  
-    if(frameCount < gifLength) {
-      capturer.capture(canvas);
-    }else if(frameCount === gifLength){
-      capturer.stop();
-      capturer.save();
-    }
+  }
+  save("mySVG.svg");
+  noLoop()
 }
