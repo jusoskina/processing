c64875f653bc55cd69dfcccf53d6a6b942c98baa
Evguenia Usoskina <jane.usoskina@gmail.com>
2020-02-27 21:40:18 +0000

    added mauer rose

--- public/mauer_rose.js
+++ public/mauer_rose.js
@@ -4,13 +4,13 @@
 // Video: https://youtu.be/f5QBExMNB1I
 // Based on: https://en.wikipedia.org/wiki/Rose_(mathematics)
 
-var d = 18;
-var n = 7;
+var d = 39;
+var n = 2;
 var sliderD;
 var sliderN;
 
 function setup() {
-  createCanvas(400, 400, SVG);
+  createCanvas(400, 400);
 //   sliderD = createSlider(1, 20, 10, 1);
 //   sliderN = createSlider(1, 20, 10, 1);
 //   sliderD.input(draw);
@@ -21,7 +21,7 @@ function draw() {
 //   d = sliderD.value();
 //   n = sliderN.value();
 //   console.log("d is: " + d + ", n is: " + n)
-  var k = n / d;
+  //var k = n / d;
   background(51);
   push();
   translate(width / 2, height / 2);
@@ -30,19 +30,37 @@ function draw() {
   stroke(255);
   noFill();
   strokeWeight(1);
-  for (var a = 0; a < TWO_PI * reduceDenominator(n, d); a += 0.02) {
-    var r = 200 * cos(k * a);
-    var x = r * cos(a);
-    var y = r * sin(a);
-    if (r >150) {
-        vertex(x, y);
-    } else {
-        vertex(-x,-y)
-    }
+ 
+  for (let i = 0; i < 361; i++) {
+    let k = i * d;
+    let r = 200 * sin(n*k);
+    // r = abs(r);
+    let x = r * cos(k);
+    let y = r * sin(k);
+    vertex(x,y);    
   }
   endShape(CLOSE);
   pop();
-  save("mySVG.svg");
+
+  // push();
+  // //translate(width / 2, height / 2);
+  // beginShape();
+  // stroke(255,0,255);
+  // noFill();
+  // strokeWeight(2);
+
+  // for (let i = 0; i < 361; i++) {
+  //   let k = i;
+  //   let r = 200 * sin(n*k);
+  //   // r = abs(r);
+  //   let x = r * cos(k);
+  //   let y = r * sin(k);
+  //   vertex(x,y);    
+  // }
+  // endShape(CLOSE);
+  // pop()
+  pop()
+  //save("flower.svg");
   noLoop();
 }
 
