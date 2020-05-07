8a41be4306885da64ace29dede6a47fd25425d1f
Jane Usoskina <janeusoskina@Janes-MBP.lan>
2020-05-07 20:29:49 +0100

    done with 4 circles, and added circle packing

--- public/3dcircles.js
+++ public/3dcircles.js
@@ -5,7 +5,7 @@ var circles;
 var spots = 400;
 
 function setup(){
-  createCanvas(840, 594);
+  createCanvas(800, 800, SVG);
     noStroke();
   //fill(255)
   noFill()
@@ -13,13 +13,13 @@ function setup(){
 }
 
 function draw(){
-    var diameter = 400
+    var diameter = 300
     var xSquare = 200
     var ySquare = 400
     var squareSize = 150
 
-    x = width/2
-    y = height/2
+    x = width*2/3
+    y = height/3
     var xS, yS
     var randR, randAng
     stroke(0)
@@ -34,39 +34,57 @@ function draw(){
       yS = randR*Math.sin(randAng)
       xSL = randR*Math.cos(randAng+randL)
       ySL = randR*Math.sin(randAng+randL)
-      //line(xS-2, yS-2, 4, 4)
-      //console.log(xS+width/2)
-      if (xS+width/2 >= xSquare&& xS+width/2 <= xSquare+squareSize && yS+ height/2 >= ySquare && yS+ height/2 <= ySquare + squareSize) {
-        console.log(xS)
-      } else if (xSL+width/2 >= xSquare&& xSL+width/2 <= xSquare+squareSize && ySL+ height/2 >= ySquare && ySL+ height/2 <= ySquare + squareSize){}
-      
-      else{
         arc(0,0, randR*2, randR*2, randAng, randAng + randL)
-      }
-      //arc(0,0, diameter/2, diameter/2, 0, PI)
-    // console.log(randR)
     }
     translate (-x, -y)
 
-
-    rect(xSquare, ySquare, squareSize, squareSize);
+    x = width/3
+    y = height/3
     translate(x,y)
     for(var i = 0; i < spots; ++i){
-      randR = randomGaussianPos(0,diameter/5, diameter/2+100)
+      randR = randomGaussianPos(0,diameter/5, diameter/2)
       randAng = Math.random() * 2*Math.PI
       randL = Math.random()*PI/2
       xS = randR*Math.cos(randAng)
       yS = randR*Math.sin(randAng)
-      //line(xS-2, yS-2, 4, 4)
-      if (xS >= xSquare && xS <= xS + squareSize && yS >= ySquare && ys <= ySquare + squareSize) {
-        console.log(xS)
+      xSL = randR*Math.cos(randAng+randL)
+      ySL = randR*Math.sin(randAng+randL)
         arc(0,0, randR*2, randR*2, randAng, randAng + randL)
-      }
-      //arc(0,0, diameter/2, diameter/2, 0, PI)
-    // console.log(randR)
     }
+    translate (-x, -y)
 
-  // save("coffeeStains.svg");
+    x = width*2/3
+    y = height*2/3
+    translate(x,y)
+    for(var i = 0; i < spots; ++i){
+      randR = randomGaussianPos(0,diameter/5, diameter/2)
+      randAng = Math.random() * 2*Math.PI
+      randL = Math.random()*PI/2
+      xS = randR*Math.cos(randAng)
+      yS = randR*Math.sin(randAng)
+      xSL = randR*Math.cos(randAng+randL)
+      ySL = randR*Math.sin(randAng+randL)
+        arc(0,0, randR*2, randR*2, randAng, randAng + randL)
+    }
+    translate (-x, -y)
+
+    x = width/3
+    y = height*2/3
+    translate(x,y)
+    for(var i = 0; i < spots; ++i){
+      randR = randomGaussianPos(0,diameter/5, diameter/2)
+      randAng = Math.random() * 2*Math.PI
+      randL = Math.random()*PI/2
+      xS = randR*Math.cos(randAng)
+      yS = randR*Math.sin(randAng)
+      xSL = randR*Math.cos(randAng+randL)
+      ySL = randR*Math.sin(randAng+randL)
+        arc(0,0, randR*2, randR*2, randAng, randAng + randL)
+    }
+    translate (-x, -y)
+
+
+  save("fourCircles.svg");
   noLoop()
 }
 
