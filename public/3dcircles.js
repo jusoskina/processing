06b6f6c4770a28316c58fcd09d553c94944c2fd8
Jane Usoskina <janeusoskina@Janes-MBP.lan>
2020-05-08 16:31:15 +0100

    bubble giraffe

--- public/3dcircles.js
+++ public/3dcircles.js
@@ -2,10 +2,10 @@ var sets = 4;
 var x;
 var y;
 var circles;
-var spots = 400;
+var spots = 200;
 
 function setup(){
-  createCanvas(800, 800, SVG);
+  createCanvas(840, 594, SVG);
     noStroke();
   //fill(255)
   noFill()
@@ -13,13 +13,13 @@ function setup(){
 }
 
 function draw(){
-    var diameter = 300
+    var diameter = 500
     var xSquare = 200
     var ySquare = 400
     var squareSize = 150
 
-    x = width*2/3
-    y = height/3
+    x = width/2
+    y = height/2
     var xS, yS
     var randR, randAng
     stroke(0)
@@ -38,53 +38,53 @@ function draw(){
     }
     translate (-x, -y)
 
-    x = width/3
-    y = height/3
-    translate(x,y)
-    for(var i = 0; i < spots; ++i){
-      randR = randomGaussianPos(0,diameter/5, diameter/2)
-      randAng = Math.random() * 2*Math.PI
-      randL = Math.random()*PI/2
-      xS = randR*Math.cos(randAng)
-      yS = randR*Math.sin(randAng)
-      xSL = randR*Math.cos(randAng+randL)
-      ySL = randR*Math.sin(randAng+randL)
-        arc(0,0, randR*2, randR*2, randAng, randAng + randL)
-    }
-    translate (-x, -y)
+    // x = width/3
+    // y = height/3
+    // translate(x,y)
+    // for(var i = 0; i < spots; ++i){
+    //   randR = randomGaussianPos(0,diameter/5, diameter/2)
+    //   randAng = Math.random() * 2*Math.PI
+    //   randL = Math.random()*PI/2
+    //   xS = randR*Math.cos(randAng)
+    //   yS = randR*Math.sin(randAng)
+    //   xSL = randR*Math.cos(randAng+randL)
+    //   ySL = randR*Math.sin(randAng+randL)
+    //     arc(0,0, randR*2, randR*2, randAng, randAng + randL)
+    // }
+    // translate (-x, -y)
 
-    x = width*2/3
-    y = height*2/3
-    translate(x,y)
-    for(var i = 0; i < spots; ++i){
-      randR = randomGaussianPos(0,diameter/5, diameter/2)
-      randAng = Math.random() * 2*Math.PI
-      randL = Math.random()*PI/2
-      xS = randR*Math.cos(randAng)
-      yS = randR*Math.sin(randAng)
-      xSL = randR*Math.cos(randAng+randL)
-      ySL = randR*Math.sin(randAng+randL)
-        arc(0,0, randR*2, randR*2, randAng, randAng + randL)
-    }
-    translate (-x, -y)
+    // x = width*2/3
+    // y = height*2/3
+    // translate(x,y)
+    // for(var i = 0; i < spots; ++i){
+    //   randR = randomGaussianPos(0,diameter/5, diameter/2)
+    //   randAng = Math.random() * 2*Math.PI
+    //   randL = Math.random()*PI/2
+    //   xS = randR*Math.cos(randAng)
+    //   yS = randR*Math.sin(randAng)
+    //   xSL = randR*Math.cos(randAng+randL)
+    //   ySL = randR*Math.sin(randAng+randL)
+    //     arc(0,0, randR*2, randR*2, randAng, randAng + randL)
+    // }
+    // translate (-x, -y)
 
-    x = width/3
-    y = height*2/3
-    translate(x,y)
-    for(var i = 0; i < spots; ++i){
-      randR = randomGaussianPos(0,diameter/5, diameter/2)
-      randAng = Math.random() * 2*Math.PI
-      randL = Math.random()*PI/2
-      xS = randR*Math.cos(randAng)
-      yS = randR*Math.sin(randAng)
-      xSL = randR*Math.cos(randAng+randL)
-      ySL = randR*Math.sin(randAng+randL)
-        arc(0,0, randR*2, randR*2, randAng, randAng + randL)
-    }
-    translate (-x, -y)
+    // x = width/3
+    // y = height*2/3
+    // translate(x,y)
+    // for(var i = 0; i < spots; ++i){
+    //   randR = randomGaussianPos(0,diameter/5, diameter/2)
+    //   randAng = Math.random() * 2*Math.PI
+    //   randL = Math.random()*PI/2
+    //   xS = randR*Math.cos(randAng)
+    //   yS = randR*Math.sin(randAng)
+    //   xSL = randR*Math.cos(randAng+randL)
+    //   ySL = randR*Math.sin(randAng+randL)
+    //     arc(0,0, randR*2, randR*2, randAng, randAng + randL)
+    // }
+    // translate (-x, -y)
 
 
-  save("fourCircles.svg");
+   save("arcCircle.svg");
   noLoop()
 }
 
---
+++ public/testingImage.js
@@ -0,0 +1,26 @@
+// 8 nights of Hanukkah 2016 Examples
+// Night 7: Animated Circle Packing
+// Expansion of: https://youtu.be/XATr_jdh-44
+// Daniel Shiffman
+// http://codingrainbow.com/
+
+// All the circles
+var circles = [];
+k = 0
+var img
+
+function preload() {
+  img = loadImage('assets/giraffe.png');
+}
+
+function setup() {
+  // createCanvas(840, 594);
+  background(img)
+}
+
+
+
+// function preload() {
+//   img = loadImage("assets/giraffe.png");
+//   console.log("jutfukf")
+// }
